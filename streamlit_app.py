9ad45f5e4e1a56a40e2832d36081eb7829efb9dc
Noman khan Durrani <159916050+NomanKhanDurranii@users.noreply.github.com>
2025-10-08 03:51:57 +0500

    Update streamlit_app.py
    added something extra

--- streamlit_app.py
+++ streamlit_app.py
@@ -43,3 +43,9 @@ if ingredients_list:
         
 
     # if ingredients_string:
+
+
+
+import requests
+smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+st.text(smoothiefroot_response)
